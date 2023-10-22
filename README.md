6e21303712b00c390e8f092d437fb55180a740d2
WJG <wjg12341234@163.com>
2023-10-22 11:05:20 +0800

    misc: init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# nested_scroll_view_plus
+
+An enhanced NestedScrollView with support for overscrolling for both the inner and outer scrollviews.
---
+++ lib/nested_scroll_view_plus.dart
@@ -0,0 +1,7 @@
+library nested_scroll_view_plus;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
